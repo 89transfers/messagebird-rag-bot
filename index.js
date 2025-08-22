e043a378110345e4fb202081676ee502c1c2b9bb
89transfers <84662234+89transfers@users.noreply.github.com>
2025-08-22 02:07:26 +0200

    feat: configure for Google Cloud Run

--- index.js
+++ index.js
@@ -91,7 +91,4 @@ app.post('/webhook', (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+exports.whatsAppWebhook = app;
